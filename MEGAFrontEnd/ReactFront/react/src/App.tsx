058fd61c11f9ce2a07475c0eb1f948c97648a060
jumpthe11@gmail.com <jumpthe11@gmail.com>
2025-03-04 11:54:18 +0300

    fixed footer and some

--- MEGAFrontEnd/ReactFront/react/src/App.tsx
+++ MEGAFrontEnd/ReactFront/react/src/App.tsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Loader from './components/loader';
 import LoginForm from './components/Login_v3/LoginForm.tsx';
 import Footer from './components/Footer/Footer.tsx'
 import NavBar2 from './components/NavBar/NavBar2.tsx'
+import CoolNavbar from './components/NavBar/CoolNavbar.tsx'
 import '../src/files/bower_components/bootstrap/css/bootstrap.min.css';
 import './files/assets/icon/font-awesome/css/font-awesome.min.css';
 import './files/assets/icon/icofont/css/icofont.css';
 import './files/assets/icon/themify-icons/themify-icons.css';
 import './files/assets/pages/waves/css/waves.min.css';
 import './files/assets/css/style.css';
+
 const App: React.FC = () => {
     const [loading, setLoading] = useState(true);
 
@@ -22,13 +24,12 @@ const App: React.FC = () => {
     }, []);
 
     if (loading) {
-        return <Loader />;
+        return <Loader/>;
     }
-    return<>
-        <NavBar2/>
+    return <>
+        <CoolNavbar/>
         <LoginForm/>
         <Footer/>
-
     </>
 };
 
