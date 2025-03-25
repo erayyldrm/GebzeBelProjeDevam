02e28415c6c598c935c9d64a137846f02e05e3d0
Mehdi Can Akbaba <mehdi.can.akbaba@gmail.com>
2025-03-25 12:49:39 +0300

    tarihiyerler sayfası

--- MEGAFrontEnd/ReactFront/react/src/components/gebze/gallarytest.tsx
+++ MEGAFrontEnd/ReactFront/react/src/components/gebze/gallarytest.tsx
@@ -5,7 +5,6 @@ import "./set2.css";
 const GalleryTest: React.FC = () => {
     return (
 
-
         <div id="pcoded" className="pcoded"><br/><br/><br/><br/><br/>
             <div className="pcoded-overlay-box"></div>
             <div className="pcoded-container navbar-wrapper">
