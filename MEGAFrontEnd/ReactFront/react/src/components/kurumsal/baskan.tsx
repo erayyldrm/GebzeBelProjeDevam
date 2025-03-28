7cd9f1184eb7f7b22272e9307a157d318efbc0fb
Mehdi Can Akbaba <mehdi.can.akbaba@gmail.com>
2025-03-28 09:44:03 +0300

    başkan sayfası responsive denendi.

--- MEGAFrontEnd/ReactFront/react/src/components/kurumsal/baskan.tsx
+++ MEGAFrontEnd/ReactFront/react/src/components/kurumsal/baskan.tsx
@@ -4,69 +4,58 @@ import { kurumsal } from "../_SayfaBilgileri/Sayfalar.tsx";
 
 const BaskanPage: React.FC = () => {
     return (
-        <div id="pcoded" className="pcoded"><br/><br/><br/><br/><br/><br/>
+        <div id="pcoded" className="pcoded">
+            <br /><br /><br /><br /><br /><br />
             <div className="pcoded-overlay-box"></div>
             <div className="pcoded-container navbar-wrapper">
-
                 <div className="pcoded-main-container">
                     <div className="pcoded-wrapper">
                         <div className="pcoded-content">
-
                             <div className="pcoded-inner-content">
                                 <div className="main-body">
                                     <div className="page-wrapper">
                                         <div className="row">
-
-                                            {/* Main container centered */}
-                                            <div className="d-flex justify-content-center w-full" style={{ minHeight: '100vh' }}>
-
-                                                {/* Content wrapper with fixed width */}
-                                                <div style={{
-                                                    width: '1200px',
-                                                    maxWidth: '100%',
-                                                    display: 'flex',
-                                                    gap: '30px'
-                                                }}>
-                                                    {/* Sidebar */}
-                                                    <div style={{ width: '25%', minWidth: '250px' }}>
-                                                        <Sidebar items={kurumsal} title={"KURUMSAL"} />
+                                            {/* Main container */}
+                                            <div className="container-fluid px-0">
+                                                <div className="row gx-lg-4">
+                                                    {/* Sidebar - becomes full width on mobile */}
+                                                    <div className="col-lg-3 col-md-4 mb-4 mb-md-0">
+                                                        <div className="sidebar-wrapper">
+                                                            <Sidebar items={kurumsal} title={"KURUMSAL"} />
+                                                        </div>
                                                     </div>
 
                                                     {/* Main Content */}
-                                                    <div style={{
-                                                        flex: 1,
-                                                        paddingTop: '20px'
-                                                    }}>
+                                                    <div className="col-lg-9 col-md-8">
                                                         <div className="card">
-                                                            <div className="card-block">
+                                                            <div className="card-block p-3 p-md-4">
                                                                 <img
                                                                     src="/images/kurumsal/baskansayfasi.jpg"
                                                                     alt="Zinnur Büyükgöz"
-                                                                    className="baskan-resim"
-                                                                    style={{ width: '100%', height: 'auto' }}
+                                                                    className="img-fluid rounded"
                                                                 />
-                                                                <br />
-                                                                <br/><h3><b>Zinnur BÜYÜKGÖZ</b></h3><br/>
-                                                                <div style={{ textAlign: 'justify' }}>
+                                                                <h3 className="mt-4 mb-3"><b>Zinnur BÜYÜKGÖZ</b></h3>
+                                                                <div className="text-justify">
                                                                     <p>
-                                                                        1964 yılında Erzurum’da doğdu. İlköğretim tahsilini 1975’te Bakırköy Koca Sinan İlkokulu’nda ve 1978’de Kadıköy İmam Hatip Ortaokulu’nda tamamladı. 1983’te ise Gebze İmam Hatip Lisesi’nden mezun oldu.
-                                                                        1987’de Yıldız Teknik Üniversitesi Mimarlık Fakültesi’nden Şehir ve Bölge Plancısı unvanıyla mezun oldu. 1987-1989 yılları arasında aynı üniversitede Şehir ve Bölge Planlaması dalında yüksek lisansını tamamladı. 33 Yıldan bu yana ise Şehir Plancısı unvanıyla hizmet etmeye devam etmektedir. Evli ve dört çocuk babasıdır.
-                                                                        Refah Partisi’nden Darıca Belde Başkanlığı ve Gebze Yönetim Kurulu Üyeliği ile Fazilet Partisi’nden Kocaeli İl Yönetimi İcra Kurulu Üyeliği yapmıştır.
+                                                                        1964 yılında Erzurum'da doğdu. İlköğretim tahsilini 1975'te Bakırköy Koca Sinan İlkokulu'nda ve 1978'de Kadıköy İmam Hatip Ortaokulu'nda tamamladı. 1983'te ise Gebze İmam Hatip Lisesi'nden mezun oldu.
+                                                                        1987'de Yıldız Teknik Üniversitesi Mimarlık Fakültesi'nden Şehir ve Bölge Plancısı unvanıyla mezun oldu. 1987-1989 yılları arasında aynı üniversitede Şehir ve Bölge Planlaması dalında yüksek lisansını tamamladı. 33 Yıldan bu yana ise Şehir Plancısı unvanıyla hizmet etmeye devam etmektedir. Evli ve dört çocuk babasıdır.
+                                                                        Refah Partisi'nden Darıca Belde Başkanlığı ve Gebze Yönetim Kurulu Üyeliği ile Fazilet Partisi'nden Kocaeli İl Yönetimi İcra Kurulu Üyeliği yapmıştır.
                                                                         2004-2009 yılları arasında Gebze Belediyesi Teknik Başkan Yardımcılığı ve Belediye Meclis Üyeliği, Kocaeli Büyükşehir Belediyesi Meclis Üyeliği ve İmar Komisyon Üyeliği görevlerinde bulunmuştur.
-                                                                        Gebze ve Kocaeli’nde siyasi görevlerde bulunmanın yanı sıra, sivil toplum kuruluşlarında ve birçok farklı platformlarda faaliyetler sürdürmüştür. 2004’ten itibaren İstanbul Kültür Varlıklarını Koruma Bölge Kurulu ile Bursa ve Kocaeli Kültür Varlıklarını Koruma Bölge Kurulları’nda üyelik yapmıştır.
-                                                                        Türkiye geneli İdare Mahkemeleri’nde Bilirkişi unvanıyla adaletin tecellisine yardımcı olmaya gayret etmiştir.
-                                                                        2014-2016 yılları arasında İstanbul Ticaret Odası Proje Danışma Kurul Üyesi olarak çalışmıştır. 2014’ten bu yana ise Teknopark İstanbul Teknoloji Bölgesi’nde Proje Danışma Kurulu Üyeliği yapmaktadır.
-                                                                        Ocak 2019 itibarıyla Kültür Varlıklarını Koruma Bölge Kurulu Üyeliği’nden ayrılmıştır.
-                                                                        31 Mart 2019 Mahalli İdareler Seçimi’nde halkımızın teveccühü ile Gebze Belediye Başkanı seçilmiştir ve halen bu görevi yürütmeye devam etmektedir.
-
-                                                                    </p><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                                                                        Gebze ve Kocaeli'nde siyasi görevlerde bulunmanın yanı sıra, sivil toplum kuruluşlarında ve birçok farklı platformlarda faaliyetler sürdürmüştür. 2004'ten itibaren İstanbul Kültür Varlıklarını Koruma Bölge Kurulu ile Bursa ve Kocaeli Kültür Varlıklarını Koruma Bölge Kurulları'nda üyelik yapmıştır.
+                                                                        Türkiye geneli İdare Mahkemeleri'nde Bilirkişi unvanıyla adaletin tecellisine yardımcı olmaya gayret etmiştir.
+                                                                        2014-2016 yılları arasında İstanbul Ticaret Odası Proje Danışma Kurul Üyesi olarak çalışmıştır. 2014'ten bu yana ise Teknopark İstanbul Teknoloji Bölgesi'nde Proje Danışma Kurulu Üyeliği yapmaktadır.
+                                                                        Ocak 2019 itibarıyla Kültür Varlıklarını Koruma Bölge Kurulu Üyeliği'nden ayrılmıştır.
+                                                                        31 Mart 2019 Mahalli İdareler Seçimi'nde halkımızın teveccühü ile Gebze Belediye Başkanı seçilmiştir ve halen bu görevi yürütmeye devam etmektedir.
+                                                                    </p>
                                                                 </div>
-                                                                <img
-                                                                    src="/images/kurumsal/imza.png"
-                                                                    alt="imza"
-                                                                    className="imza"
-                                                                    style={{ width: '200px', height: '200px', float: 'right', marginTop: '20px' }}
-                                                                />
+                                                                <div className="text-center text-md-end mt-4">
+                                                                    <img
+                                                                        src="/images/kurumsal/imza.png"
+                                                                        alt="imza"
+                                                                        className="img-fluid"
+                                                                        style={{ maxWidth: '200px', height: 'auto' }}
+                                                                    />
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
@@ -80,6 +69,28 @@ const BaskanPage: React.FC = () => {
                     </div>
                 </div>
             </div>
+
+            {/* Responsive CSS */}
+            <style jsx>{`
+                @media (max-width: 767.98px) {
+                    .sidebar-wrapper {
+                        margin-bottom: 2rem;
+                    }
+                    .card-block {
+                        padding: 1.5rem !important;
+                    }
+                }
+                @media (min-width: 768px) and (max-width: 991.98px) {
+                    .col-md-4 {
+                        flex: 0 0 35%;
+                        max-width: 35%;
+                    }
+                    .col-md-8 {
+                        flex: 0 0 65%;
+                        max-width: 65%;
+                    }
+                }
+            `}</style>
         </div>
     );
 };
