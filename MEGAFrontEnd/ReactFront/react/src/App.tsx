492973926094723ebe0733275c320578ee300f8e
jumpthe11@gmail.com <jumpthe11@gmail.com>
2025-04-11 15:22:20 +0300

    Apptsx

--- MEGAFrontEnd/ReactFront/react/src/App.tsx
+++ MEGAFrontEnd/ReactFront/react/src/App.tsx
@@ -56,6 +56,7 @@ const App: React.FC = () => {
                     <Route path="/test" element={<TEST/>}></Route>
 
                     <Route path="/etkinlikler" element={<EventsSection/>}/>
+                    {/* Kurumsal gibi gebzede olması lazım */}
                     <Route
                         path="/kurumsal/*"
                         element={<KurumsalLayout />}
