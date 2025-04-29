8b657f19a014f465a26e1055d8e43cb19b295046
jumpthe11@gmail.com <jumpthe11@gmail.com>
2025-04-29 15:23:42 +0300

    Kullanıcı backend api düzeltildi

--- src/main/java/com/kocaeli/bel/DTO/UserDTO.java
+++ src/main/java/com/kocaeli/bel/DTO/UserDTO.java
@@ -5,25 +5,25 @@ import lombok.*;
 @Getter
 @Setter
 public class UserDTO {
-    private Long idno;
+    private Long id;
     private String tcno;
     private String password;
     private String role;
 
     public UserDTO(String tcno, Long idno, String password, String role) {
         this.tcno = tcno;
-        this.idno = idno;
+        this.id = idno;
         this.password = password;
         this.role = role;
     }
 
     public UserDTO(Long idno, String tcno) {
-        this.idno = idno;
+        this.id = idno;
         this.tcno = tcno;
     }
 
     public UserDTO(Long idno, String tcno, String role) {
-        this.idno = idno;
+        this.id = idno;
         this.tcno = tcno;
         this.role = role;
     }
