1447c649d9ebdc96a5b93c605028f1f8823fa0be
jumpthe11@gmail.com <jumpthe11@gmail.com>
2025-05-06 16:24:44 +0300

    sayfalar databaseye aktarılıyor

--- src/main/java/com/kocaeli/bel/service/CustomUserDetailsService.java
+++ src/main/java/com/kocaeli/bel/service/CustomUserDetailsService.java
@@ -1,6 +1,6 @@
 package com.kocaeli.bel.service;
 
-import com.kocaeli.bel.model.User;
+import com.kocaeli.bel.entity.User;
 import com.kocaeli.bel.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
