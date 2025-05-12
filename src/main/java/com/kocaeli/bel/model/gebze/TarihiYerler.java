ddd9ae215a99469375133d990b361fb6b71da7c5
jumpthe11@gmail.com <jumpthe11@gmail.com>
2025-05-12 12:07:46 +0300

    Tarihi yerler limit kaldırıldı

--- src/main/java/com/kocaeli/bel/model/gebze/TarihiYerler.java
+++ src/main/java/com/kocaeli/bel/model/gebze/TarihiYerler.java
@@ -17,13 +17,13 @@ public class TarihiYerler {
     @Column(name = "RESIM_URL") // Ana görselin URL'si
     private String resimUrl;
 
-    @Column(name = "YER_ISMI", length = 150) // Tarihi yerin adı, max 150 karakter
+    @Column(name = "YER_ISMI") // Tarihi yerin adı, max 150 karakter
     private String yerIsmi;
 
     @Column(name = "GENEL_BILGI") // Genel açıklama veya tanıtım metni
     private String genelBilgi;
 
-    @Column(name = "KONUM", length = 500) // Yerleşim bilgisi veya adres, max 500 karakter
+    @Column(name = "KONUM") // Yerleşim bilgisi veya adres, max 500 karakter
     private String konum;
 
     @Column(name = "AKTIVITELER") // Alanda yapılabilecek aktiviteler
@@ -32,6 +32,6 @@ public class TarihiYerler {
     @Column(name = "NASIL_GIDILIR") // Ulaşım bilgileri (otobüs, araba vs.)
     private String nasilGidilir;
 
-    @Column(name = "GALERI", length = 2000) // Galeri görselleri (JSON formatında veya virgülle ayrılmış URL dizisi)
+    @Column(name = "GALERI") // Galeri görselleri (JSON formatında veya virgülle ayrılmış URL dizisi)
     private String galeri;
 }
